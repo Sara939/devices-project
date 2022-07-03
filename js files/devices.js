31f5b2cf1d8b57913f80bab7e62a59686a6478dd
sara-likaount <saralikaunt@gmail.com>
2022-07-03 20:05:08 +0300

    n

--- js files/devices.js
+++ js files/devices.js
@@ -1,14 +1,48 @@
-async function getdeviceinfo(){
-    try{
+async function getdeviceinfo() {
+    try {
         return await fetch("https://my-json-server.typicode.com/Jeck99/fake-server/devices")
-        .then(res=> res.json())
+            .then(res => res.json())
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
-    finally{}
+    finally { }
 }
 
-function printdevices(){
-    getdeviceinfo().then(answer=>info1.innerHTML= answer)
+// function printdevices(){
+
+//     getdeviceinfo().then((answer)=> {answer.forEach(element => {   
+
+//         for (const key in element) {
+//             main.innerHTML+= `${key} : ${element[key]}` ;
+//         }
+// })})
+
+// }
+// printdevices();
+
+
+
+
+function printdevices() {
+
+    getdeviceinfo().then((answer) => {answer.forEach(element => {
+    row_container.innerHTML += `<div class="col-xs-12 col-md-3 ">
+    ${element.price} ${element.isAvailable} ${element.createdAt}
+    ${element.color} ${element.brand} ${element.ram}
+    <img class="img-thumbnail" src="${element.picture}"/></div>`
+        });
+    })
+}
+
+printdevices()
+
+
+async function deletdevice(){
+    try{
+        return await fetch()
+    }
+    catch{}
+    finally{}
+
 }
