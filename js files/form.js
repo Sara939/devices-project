ec68eccae5b4b74676f12454d8edc17287b300a2
sara-likaount <saralikaunt@gmail.com>
2022-07-04 20:20:33 +0300

    form done

--- js files/form.js
+++ js files/form.js
@@ -19,7 +19,7 @@ async function postdevice() {
             headers:{
                 'Content-Type': 'application/json'
             }
-        })
+        }).then(res=>alert("Sent Successfully"))
         
     }
     catch (err) {
@@ -27,41 +27,3 @@ async function postdevice() {
     }
     finally {}
 }
-
-
-
-
-
-
-
-
-
-
-// function printdevices() {
-
-//     getdeviceinfo().then((answer) => {answer.forEach(element => {
-//     row_container.innerHTML += `<div class="col-3" id="${element.id}">
-//     <img class="img-thumbnail" src="${element.picture}"/>
-//     ${element.price} ${element.isAvailable} ${element.createdAt}
-//     ${element.color} ${element.brand} ${element.ram}
-    
-//     <button onclick="deletdevice('${element.id}')">&#9940;</button>
-//     </div>
-//     `
-//         });
-//     })
-// }
-
-// printdevices()
-
-
-
-// async function deletdevice(id){
-//     try{
-//         let result = await fetch(`${API}/${id}`, {method: "DELETE"})
-//         if(result.status <= 200) document.getElementById(id).remove()
-//     }
-//     catch(err){console.log(err)}
-//     finally{}
-
-// }
