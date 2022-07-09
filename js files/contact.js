e5ac4f6ecf8df024fb0f14d9afbe3d95d193ac16
sara-likaount <saralikaunt@gmail.com>
2022-07-09 23:24:53 +0300

    email

--- js files/contact.js
+++ js files/contact.js
@@ -1,6 +1,6 @@
 function sendtomyemail() {
     Email.send({
-        Host : "smtp.elasticemail.com",
+        Host : "smtp.gmail.com",
         Username : "saralikaount@gmail.com",
         Password : "C7324DF20242A6CE792C8EA74088320A3365",
         To : 'saralikaount@gmail.com',
